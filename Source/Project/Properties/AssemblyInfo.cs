c6a7b319e0fc1480f1617556ecd9d342bcc6affa
Lisias T <me@lisias.net>
2018-08-29 16:00:16 -0300

    Versioning Normalizing

--- Source/Project/Properties/AssemblyInfo.cs
+++ Source/Project/Properties/AssemblyInfo.cs
@@ -17,9 +17,9 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("3.0.3")]
-[assembly: AssemblyFileVersion("3.0.3")]
-[assembly: AssemblyInformationalVersionAttribute("3.0.3")]
+[assembly: AssemblyVersion(KerbalStats.Version.Number)]
+[assembly: AssemblyFileVersion(KerbalStats.Version.Number)]
+[assembly: AssemblyInformationalVersionAttribute(KerbalStats.Version.Number)]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
